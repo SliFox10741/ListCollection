e30ef107a44061416640a258fff125705951e750
Maxim <mr.maximgus@gmail.com>
2022-12-17 15:33:42 +0500

    Окончание дополнительного задания

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
         String product = null;
         ArrayList products = new ArrayList<>();
 
-
         System.out.println("""
                 Список операций:\s
                 1. Добавить в список
@@ -22,50 +21,62 @@ public class Main {
             input = scanner.nextLine();
             if (input.equals("end"))
                 break;
-            switch (Integer.parseInt(input)){
-                case 1:
+            switch (Integer.parseInt(input)) {
+                case 1 -> {
                     System.out.println("Какую покупку хотите добавить?");
                     product = scanner.nextLine();
                     products.add(product);
                     System.out.println("Итого в списке покупок: " + products.size());
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     System.out.println("Список покупок:");
                     showProduct(products);
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     showProduct(products);
                     System.out.println("Какую хотите удалить? Введите номер или название");
                     int index;
                     product = scanner.nextLine();
                     if (isNumeric(product)) {
                         index = Integer.parseInt(product);
-                        if (products.size() >= index && index > 0 ){
-                            products.remove(index-1);
+                        if (products.size() >= index && index > 0) {
+                            products.remove(index - 1);
                             System.out.println("Покупка " + product + " удалена, список продуктов: ");
-                        }else {
+                        } else {
                             System.out.println("Такого продукта нет в списке, список продуктов: ");
                         }
-                    } else{
-                        if (products.contains(product)){
+                    } else {
+                        if (products.contains(product)) {
                             index = products.indexOf(product);
                             products.remove(index);
                             System.out.println("Покупка " + product + " удалена, список продуктов: ");
-                        }else {
+                        } else {
                             System.out.println("Такого продукта нет в списке, список продуктов: ");
                         }
                     }
                     showProduct(products);
-                    break;
-                default:
-                    System.out.println("Такой операции нет! Попробуйте ещё раз.");
+                }
+                case 4 -> {
+                    System.out.println("Введите текст для поиска: ");
+                    String queryLower = scanner.nextLine().toLowerCase();
+                    String itemLower = null;
+                    System.out.println("Найдено: \n");
+                    for (int i = 0; i < products.size(); i++) {
+                        itemLower = (String) products.get(i);
+                        itemLower = itemLower.toLowerCase();
+                        if (itemLower.contains(queryLower)) {
+                            System.out.println((i + 1) + ". " + products.get(i));
+                        }
+                    }
+                }
+                default -> System.out.println("Такой операции нет! Попробуйте ещё раз.");
             }
         }
     }
 
     public static void showProduct(ArrayList<String> products) {
         for (int i = 0; i < products.size(); i++) {
-            System.out.println((i+1) + ". " + products.get(i));
+            System.out.println((i + 1) + ". " + products.get(i));
         }
     }
 
@@ -73,7 +84,7 @@ public class Main {
         try {
             Double.parseDouble(str);
             return true;
-        } catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             return false;
         }
     }
